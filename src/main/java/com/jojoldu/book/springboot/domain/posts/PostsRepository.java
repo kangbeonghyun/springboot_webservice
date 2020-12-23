052da2209b81e2f27ff04794363fd47710b1b62f
kangbeonghyun <zxcvb8802@naver.com>
2020-12-24 00:10:54 +0900

    TIL mustache 20201223

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,5 +1,10 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts,Long> {//<Entitity class, PK타입> 이렇게 상속하면 CRUD메소드 자동 생성
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.service.posts;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.stream.Collectors;
+import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -31,4 +34,19 @@ public class PostsService {
         Posts entity=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글 없음. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()//postsrepository에서 넘어온 posts의 스트림을 map을통해 postslistresponsedto->list로 반환하는 것.
+                .map(PostsListResponseDto::new)//람다식, 원래는 .map(posts->new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글 없음 id="+id));
+        postsRepository.delete(posts);
+    }
+
+
 }
